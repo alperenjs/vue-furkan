13b47f486c194f6f231364924911c82b126244fb
alperenkarate <alperensozen96@gmail.com>
2021-12-18 15:05:07 +0300

    task is updated.

--- vue-task-api/index.js
+++ vue-task-api/index.js
@@ -12,3 +12,7 @@ var vueTask = new Vue({
     //componentler render edildiği an
   }
 })
+
+//TASK
+//axios ile veriyi çek (posts verisi)
+//bootstrap card içine v-for ile dön veri title and body yazdır >> https://getbootstrap.com/docs/4.0/components/card/ d-flex olsun güzel gözüksün
