6c848ba39fc391d9b5aa072e3fe8bf5ce92b5abe
alperenkarate <alperensozen96@gmail.com>
2021-12-18 15:02:18 +0300

    vue task is added.

---
+++ vue-task-api/index.js
@@ -0,0 +1,14 @@
+var vueTask = new Vue({
+  el: '#app',
+  data: {
+    message: 'Selam',
+    api: 'https://jsonplaceholder.typicode.com/posts'
+  },
+  methods: {},
+  created() {
+    //apiden veri çekme işlemleri burada olur
+ },
+  mounted() {
+    //componentler render edildiği an
+  }
+})
